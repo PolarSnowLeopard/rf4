fca791265e658dddee14500a0b299915dde19eba
PolarSnowLeopard <1365240381@qq.com>
2025-05-15 18:15:02 +0800

    feat: 取消鉴权

--- app/rf4/settings.py
+++ app/rf4/settings.py
@@ -36,9 +36,9 @@ ALLOWED_HOSTS = ['fdueblab.cn', 'localhost', '127.0.0.1']
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    # ]
 }
 
 # Application definition
